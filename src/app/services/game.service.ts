2046774d502b5f313ccb57770da45c3e8ad5f547
Laroki <rami.cakir01@gmail.com>
2025-09-16 20:12:52 +0200

    create game service

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  private http = inject(HttpClient);
+
+  createGame(isPrivate: boolean): Observable<any> {
+    return this.http.post(`${environment.api}/game/create`, { isPrivate })
+  }
+}
