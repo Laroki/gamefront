6c8a20678dcd2191f4e638c6573ecb43918c50ac
Laroki <rami.cakir01@gmail.com>
2025-09-17 01:43:04 +0200

    add type for create and join game and add leaveGame function

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { Game } from '../interfaces/game.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,15 @@ export class GameService {
 
   private http = inject(HttpClient);
 
-  createGame(isPrivate: boolean): Observable<any> {
-    return this.http.post(`${environment.api}/game/create`, { isPrivate })
+  createGame(isPrivate: boolean): Observable<Game> {
+    return this.http.post(`${environment.api}/game/create`, { isPrivate }) as Observable<Game>
   }
 
-  joinGame(gameID: string): Observable<any> {
-    return this.http.patch(`${environment.api}/game/${gameID}/join`, {})
+  joinGame(gameID: string): Observable<Game> {
+    return this.http.patch(`${environment.api}/game/${gameID}/join`, {}) as Observable<Game>
+  }
+
+  leaveGame(gameID: string): Observable<Game> {
+    return this.http.patch(`${environment.api}/game/${gameID}/leave`, {}) as Observable<Game>
   }
 }
