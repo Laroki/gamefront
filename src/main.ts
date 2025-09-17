9e36ccdacc971abbb4b49deb1d99635cad1f182a
Laroki <rami.cakir01@gmail.com>
2025-09-17 18:52:49 +0200

    add binding for getting route params

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AuthInterceptor } from './app/auth/auth.interceptor';
 
@@ -10,7 +10,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideAnimations()
   ]
 });
