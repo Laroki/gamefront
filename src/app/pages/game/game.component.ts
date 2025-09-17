126fbcf4336945c325664f51dc4e456835023044
Laroki <rami.cakir01@gmail.com>
2025-09-17 18:53:55 +0200

    create base game component

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  private gameService = inject(GameService)
+  private router = inject(Router)
 
+  id = input.required<string>()
+
+  ngOnInit(): void {
+    this.gameService.getGameById(this.id()).subscribe({
+      next: (res) => console.log(res)
+    })
+  }
+
+  leaveGame() {
+    this.gameService.leaveGame(this.id()).subscribe({
+      next: () => this.router.navigateByUrl('/dashboard'),
+      error: (err) => console.log(err)
+    })
+  }
 }
