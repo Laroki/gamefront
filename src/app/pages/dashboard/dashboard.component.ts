2cdd2b823b05c5d019dfa3a362ffa35be07bc8e9
Laroki <rami.cakir01@gmail.com>
2025-09-17 18:54:06 +0200

    create dashboard base

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TitleCasePipe, CommonModule } from '@angular/common';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+
 import { AuthService } from '../../services/auth.service';
+import { GameService } from '../../services/game.service';
+import { UserService } from '../../services/user.service';
+import { timeAgoPipe } from '../../pipes/time-ago.pipe';
+import { Game } from '../../interfaces/game.interface';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, FormsModule, MatInputModule, TitleCasePipe, CommonModule, timeAgoPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private authService = inject(AuthService)
+  private gameService = inject(GameService)
+  userService = inject(UserService)
+  private router = inject(Router)
 
-  constructor(private authService: AuthService) { }
+  isPrivate = true;
+  gameID = '';
+  username = this.userService.getUser()?.username
+  activeGames: Game[] = []
+  publicGames$ = this.gameService.getPublicWaitingGames()
+  noGameFound = false
+
+  ngOnInit() {
+    this.userService.getActiveGames().subscribe(games => {
+      this.activeGames = games;
+    });
+  }
+
 
   logout() {
     this.authService.logout()
   }
 
+  createGame() {
+    this.gameService.createGame(this.isPrivate).subscribe({
+      next: (res) => this.router.navigate(['/game', res.id]),
+      error: (err) => console.error('Erreur login:', err)
+    })
+  }
+
+  joinGameByID(gameId: string) {
+    this.noGameFound = false
+    this.gameService.joinGame(gameId).subscribe({
+      next: (res) => this.router.navigate(['/game', res.id]),
+      error: () => this.noGameFound = true
+    })
+  }
+
+  joinActiveGame(id: string) {
+    this.router.navigate(['/game', id])
+  }
+
+  leaveGame(gameID: string) {
+    this.gameService.leaveGame(gameID).subscribe({
+      next: () => this.activeGames = this.activeGames.filter(g => g.id !== gameID)
+    })
+  }
 }
