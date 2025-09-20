389c9340ed793c36744431a8bdcfd4aa0f338cde
Laroki <rami.cakir01@gmail.com>
2025-09-20 20:45:55 +0200

    add new keys in interface

--- src/app/interfaces/game.interface.ts
+++ src/app/interfaces/game.interface.ts
@@ -1,3 +1,4 @@
+import { Round } from "./round.interface";
 import { User } from "./user.interface";
 
 export interface Game {
@@ -8,7 +9,9 @@ export interface Game {
     player1: User,
     player2?: User,
     abandonedBy: User,
-    role?: GameRole
+    role?: GameRole,
+    rounds?: Round[],
+    userSentWord?: boolean
 }
 
 export enum GameRole {
@@ -22,4 +25,5 @@ export enum GameStatus {
     IN_PROGRESS = 'in_progress',
     FINISHED = 'finished',
     ABANDONED = 'abandoned',
+    WON = 'won'
 }
